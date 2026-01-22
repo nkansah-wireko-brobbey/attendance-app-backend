ad01d3b8e249f7782d2da4f182705e95ed4706a7
Nkansah <nkansahwirekobrobbey@gmail.com>
2026-01-21 21:20:46 -0600

    feat: implement retrieve all usersendpoint

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -6,13 +6,11 @@ import com.example.demo.dto.UserDto;
 import com.example.demo.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,8 +28,8 @@ public class UserController {
 
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<List<UserDto>> getUsers(){
-
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.getAllUsers());
     }
 }
